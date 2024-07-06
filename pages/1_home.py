0d2f350f41bcd53582c1fe8386d9a86e0a36fafd
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-07-06 10:25:57 -0700

    openai setup

--- pages/1_home.py
+++ pages/1_home.py
@@ -1,3 +1,19 @@
 import streamlit as st
+import time
 
-st.write('home')
+st.title("Product Copy Generator")
+st.write("Generate catchy copy for your product!")
+
+
+product_name = st.text_input("Product Name")
+product_description = st.text_area("Product Description")
+
+
+if st.button("Generate Copy", type="primary"):
+    if product_name and product_description:
+        with st.spinner("Generating magic...."):
+            time.sleep(5)
+            st.write("LLM will generate value")
+
+    else:
+        st.write("You need to ask me something.")
