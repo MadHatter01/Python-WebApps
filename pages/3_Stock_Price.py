ce4591727faaf1a35a036470c746252eb74e0000
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-07-09 21:50:33 -0700

    figure styles

--- pages/3_Stock_Price.py
+++ pages/3_Stock_Price.py
@@ -14,11 +14,32 @@ df = data.history(period="1mo")
 st.subheader(f"Stock data for {stock_symbol.upper()}")
 st.write(df)
 
-st.subheader("Volume Chart")
-fig, ax = plt.subplots()
-ax.bar(df.index, df['Volume'])
-ax.set_xlabel('Date')
-plt.xticks(rotation=45)
-ax.set_ylabel('Volume')
-ax.set_title(f'Trading volume of {stock_symbol.upper()} over the last one month')
-st.pyplot(fig)
+col1, col2 = st.columns(2)
+with col1:
+    st.subheader("Volume Chart")
+    fig, ax = plt.subplots()
+    ax.bar(df.index, df['Volume'])
+    ax.set_xlabel('Date')
+    plt.xticks(rotation=45)
+    ax.set_ylabel('Volume')
+    ax.set_title(f'Trading volume of {stock_symbol.upper()} over the last one month')
+    st.pyplot(fig)
+    st.bar_chart(df['Volume'])
+    
+    
+
+
+
+with col2:
+    
+    st.subheader("Closing Price Chart")
+    fig, ax = plt.subplots()
+
+    ax.plot(df.index, df['Close'], marker='o')
+    ax.set_xlabel('Date')
+    ax.set_ylabel('Closing Price')
+    ax.set_title(f'Closing price of {stock_symbol.upper()} over the last one month')
+    plt.xticks(rotation=45)
+    st.pyplot(fig)
+    st.line_chart(df['Close'])
+    
