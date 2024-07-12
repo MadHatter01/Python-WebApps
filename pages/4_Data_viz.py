70db95ca534a82188fd2987c9feb76d124ed2228
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-07-11 18:50:46 -0700

    petal visualization

--- pages/4_Data_viz.py
+++ pages/4_Data_viz.py
@@ -14,4 +14,32 @@ fig = gobj.Figure()
 fig.add_trace(gobj.Scatter(x=x, y=y_sin, mode='lines', name='Sine Wave'))
 fig.add_trace(gobj.Scatter(x=x, y=y_cos, mode='lines', name='Cos Wave'))
 
+
+st.plotly_chart(fig)
+
+
+
+st.title("Interactive flower curve viz")
+
+n = st.slider("Select n (number of petals if n is odd, 2n if even)", 1, 10, 4)
+d = st.slider("Select d (density of petals)", 1, 10, 6)
+
+theta = np.linspace(0, 2 * np.pi, 1000)
+r = np.cos(n * theta / d)
+
+x = r * np.cos(theta)
+y = r * np.sin(theta)
+
+fig = gobj.Figure()
+
+fig.add_trace(gobj.Scatter(x=x, y=y, mode='lines', name='Rose Curve'))
+
+fig.update_layout(title="Flower Curve",
+                  xaxis=dict(visible=False),
+                  yaxis=dict(visible=False),
+                  showlegend=False,
+                  plot_bgcolor='black',
+                  paper_bgcolor='black',
+                  title_font_color='white')
+
 st.plotly_chart(fig)
