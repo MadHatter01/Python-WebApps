777c134fe5598ef05fdd2e83765c8fcbfc1d0d50
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-07-25 19:35:35 -0700

    translate text to audio logic

--- pages/16_Text_Speech.py
+++ pages/16_Text_Speech.py
@@ -6,4 +6,15 @@ import streamlit.components.v1 as components
 st.set_page_config(page_title="Audio Generator", page_icon=":microphone:")
 user_input = st.text_area("Enter text to generate speech:")
 
-st.write(user_input)
+if st.button("Generate Audio"):
+    if user_input:
+        tts = gTTS(text=user_input, lang="en")
+        tts.save("output.mp3")
+
+        audio_file = open("output.mp3", "rb")
+        audio_bytes = audio_file.read()
+
+
+        st.audio(audio_bytes, format = "audio/mp3")
+
+    
